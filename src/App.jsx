42870aca8525a9a5f3b9f9021132aec3273b93f3
Isaac Sung <isaacsung@gmail.com>
2021-08-12 17:17:01 -0500

    Update App.jsx to show main menu.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
-import TempGame from "./components/temp-game.component";
+import MainMenu from "./components/main-menu.component";
+import TempGuessGame from "./components/temp-guess-game";
 
 function App() {
   return (
@@ -11,7 +12,16 @@ function App() {
           <div className="container">
               <Navbar />
               <br/>
-              <Route path="/" exact component={TempGame} />
+              <Route path="/" exact component={MainMenu} />
+              <Route path="/cities" exact render={(props) => (
+                <TempGuessGame mode="cities" />
+              )} />
+              <Route path="/maps" exact render={(props) => (
+                  <TempGuessGame mode="maps" />
+              )} />
+              <Route path="/citiesandmaps" exact render={(props) => (
+                  <TempGuessGame mode="citiesandmaps" />
+              )} />
           </div>
       </Router>
   );
