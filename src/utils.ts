bf694559165239d65d12b2992d2b941c2e4531b4
Isaac Sung <isaacsung@gmail.com>
2025-06-06 19:27:05 -0500

    add a bunch more cities

--- src/utils.ts
+++ src/utils.ts
@@ -37,7 +37,7 @@ export const cities = [
 		lat: -33.8698439,
 		lon: 151.2082848
 	},
-	{ name: 'Dubai', country: 'UAE', countryCode: '', temp: 35, lat: 25.2653471, lon: 55.2924914 },
+	{ name: 'Dubai', country: 'UAE', countryCode: 'AE', temp: 35, lat: 25.2653471, lon: 55.2924914 },
 	{
 		name: 'Moscow',
 		country: 'Russia',
@@ -50,7 +50,7 @@ export const cities = [
 	{
 		name: 'Paris',
 		country: 'France',
-		countryCode: '',
+		countryCode: 'FR',
 		temp: 16,
 		lat: 48.8588897,
 		lon: 2.3200410217200766
@@ -110,6 +110,601 @@ export const cities = [
 		temp: 12,
 		lat: 49.2608724,
 		lon: -123.113952
+	},
+	// Additional cities with accurate coordinates
+	{
+		name: 'Berlin',
+		country: 'Germany',
+		countryCode: 'DE',
+		temp: 10,
+		lat: 52.5170365,
+		lon: 13.3888599
+	},
+	{
+		name: 'Rome',
+		country: 'Italy',
+		countryCode: 'IT',
+		temp: 19,
+		lat: 41.8933203,
+		lon: 12.4829321
+	},
+	{
+		name: 'Madrid',
+		country: 'Spain',
+		countryCode: 'ES',
+		temp: 17,
+		lat: 40.4167047,
+		lon: -3.7035825
+	},
+	{
+		name: 'Amsterdam',
+		country: 'Netherlands',
+		countryCode: 'NL',
+		temp: 11,
+		lat: 52.3727598,
+		lon: 4.8936041
+	},
+	{
+		name: 'Vienna',
+		country: 'Austria',
+		countryCode: 'AT',
+		temp: 12,
+		lat: 48.2083537,
+		lon: 16.3725042
+	},
+	{
+		name: 'Prague',
+		country: 'Czech Republic',
+		countryCode: 'CZ',
+		temp: 9,
+		lat: 50.0874654,
+		lon: 14.4212535
+	},
+	{
+		name: 'Warsaw',
+		country: 'Poland',
+		countryCode: 'PL',
+		temp: 8,
+		lat: 52.2319581,
+		lon: 21.0067249
+	},
+	{
+		name: 'Helsinki',
+		country: 'Finland',
+		countryCode: 'FI',
+		temp: 5,
+		lat: 60.1674881,
+		lon: 24.9427473
+	},
+	{
+		name: 'Oslo',
+		country: 'Norway',
+		countryCode: 'NO',
+		temp: 6,
+		lat: 59.9133301,
+		lon: 10.7389701
+	},
+	{
+		name: 'Copenhagen',
+		country: 'Denmark',
+		countryCode: 'DK',
+		temp: 9,
+		lat: 55.6867243,
+		lon: 12.5700724
+	},
+	{
+		name: 'Zurich',
+		country: 'Switzerland',
+		countryCode: 'CH',
+		temp: 10,
+		lat: 47.3744489,
+		lon: 8.5410422
+	},
+	{
+		name: 'Brussels',
+		country: 'Belgium',
+		countryCode: 'BE',
+		temp: 11,
+		lat: 50.8465573,
+		lon: 4.351697
+	},
+	{
+		name: 'Lisbon',
+		country: 'Portugal',
+		countryCode: 'PT',
+		temp: 18,
+		lat: 38.7077507,
+		lon: -9.1365919
+	},
+	{
+		name: 'Dublin',
+		country: 'Ireland',
+		countryCode: 'IE',
+		temp: 10,
+		lat: 53.3498006,
+		lon: -6.2602964
+	},
+	{
+		name: 'Edinburgh',
+		country: 'Scotland',
+		countryCode: 'GB',
+		temp: 9,
+		lat: 55.9533456,
+		lon: -3.1883749
+	},
+	{
+		name: 'Buenos Aires',
+		country: 'Argentina',
+		countryCode: 'AR',
+		temp: 17,
+		lat: -34.6118296,
+		lon: -58.3960833
+	},
+	{
+		name: 'Santiago',
+		country: 'Chile',
+		countryCode: 'CL',
+		temp: 15,
+		lat: -33.4377968,
+		lon: -70.6504451
+	},
+	{
+		name: 'Lima',
+		country: 'Peru',
+		countryCode: 'PE',
+		temp: 19,
+		lat: -12.0621065,
+		lon: -77.0365256
+	},
+	{
+		name: 'Bogotá',
+		country: 'Colombia',
+		countryCode: 'CO',
+		temp: 14,
+		lat: 4.6533816,
+		lon: -74.0836333
+	},
+	{
+		name: 'Mexico City',
+		country: 'Mexico',
+		countryCode: 'MX',
+		temp: 16,
+		lat: 19.4326296,
+		lon: -99.1331785
+	},
+	{
+		name: 'Los Angeles',
+		country: 'USA',
+		countryCode: 'US',
+		temp: 20,
+		lat: 34.0536909,
+		lon: -118.242766
+	},
+	{
+		name: 'Chicago',
+		country: 'USA',
+		countryCode: 'US',
+		temp: 10,
+		lat: 41.8755616,
+		lon: -87.6244212
+	},
+	{
+		name: 'San Francisco',
+		country: 'USA',
+		countryCode: 'US',
+		temp: 15,
+		lat: 37.7792588,
+		lon: -122.4193286
+	},
+	{
+		name: 'Toronto',
+		country: 'Canada',
+		countryCode: 'CA',
+		temp: 9,
+		lat: 43.6534817,
+		lon: -79.3839347
+	},
+	{
+		name: 'Montreal',
+		country: 'Canada',
+		countryCode: 'CA',
+		temp: 6,
+		lat: 45.5031824,
+		lon: -73.5698065
+	},
+	{
+		name: 'Beijing',
+		country: 'China',
+		countryCode: 'CN',
+		temp: 12,
+		lat: 39.9044516,
+		lon: 116.4073963
+	},
+	{
+		name: 'Shanghai',
+		country: 'China',
+		countryCode: 'CN',
+		temp: 17,
+		lat: 31.2252985,
+		lon: 121.4767923
+	},
+	{
+		name: 'Hong Kong',
+		country: 'Hong Kong',
+		countryCode: 'HK',
+		temp: 24,
+		lat: 22.2793278,
+		lon: 114.1628131
+	},
+	{
+		name: 'Seoul',
+		country: 'South Korea',
+		countryCode: 'KR',
+		temp: 13,
+		lat: 37.5666791,
+		lon: 126.9782914
+	},
+	{
+		name: 'Osaka',
+		country: 'Japan',
+		countryCode: 'JP',
+		temp: 16,
+		lat: 34.6936312,
+		lon: 135.5021651
+	},
+	{
+		name: 'Manila',
+		country: 'Philippines',
+		countryCode: 'PH',
+		temp: 28,
+		lat: 14.5906216,
+		lon: 120.9799696
+	},
+	{
+		name: 'Jakarta',
+		country: 'Indonesia',
+		countryCode: 'ID',
+		temp: 28,
+		lat: -6.2146067,
+		lon: 106.8451443
+	},
+	{
+		name: 'Kuala Lumpur',
+		country: 'Malaysia',
+		countryCode: 'MY',
+		temp: 28,
+		lat: 3.1516964,
+		lon: 101.6942371
+	},
+	{
+		name: 'Ho Chi Minh City',
+		country: 'Vietnam',
+		countryCode: 'VN',
+		temp: 28,
+		lat: 10.7763897,
+		lon: 106.7011391
+	},
+	{
+		name: 'Delhi',
+		country: 'India',
+		countryCode: 'IN',
+		temp: 25,
+		lat: 28.6138954,
+		lon: 77.2090057
+	},
+	{
+		name: 'Kolkata',
+		country: 'India',
+		countryCode: 'IN',
+		temp: 27,
+		lat: 22.5677459,
+		lon: 88.3476023
+	},
+	{
+		name: 'Chennai',
+		country: 'India',
+		countryCode: 'IN',
+		temp: 29,
+		lat: 13.0843007,
+		lon: 80.2704622
+	},
+	{
+		name: 'Bangalore',
+		country: 'India',
+		countryCode: 'IN',
+		temp: 24,
+		lat: 12.9767936,
+		lon: 77.590082
+	},
+	{
+		name: 'Karachi',
+		country: 'Pakistan',
+		countryCode: 'PK',
+		temp: 26,
+		lat: 24.8546842,
+		lon: 67.0207055
+	},
+	{
+		name: 'Istanbul',
+		country: 'Turkey',
+		countryCode: 'TR',
+		temp: 15,
+		lat: 41.0096334,
+		lon: 28.9651646
+	},
+	{
+		name: 'Tehran',
+		country: 'Iran',
+		countryCode: 'IR',
+		temp: 16,
+		lat: 35.6892523,
+		lon: 51.3896004
+	},
+	{
+		name: 'Riyadh',
+		country: 'Saudi Arabia',
+		countryCode: 'SA',
+		temp: 26,
+		lat: 24.6408305,
+		lon: 46.7727951
+	},
+	{
+		name: 'Tel Aviv',
+		country: 'Israel',
+		countryCode: 'IL',
+		temp: 20,
+		lat: 32.0852997,
+		lon: 34.7818064
+	},
+	{
+		name: 'Cape Town',
+		country: 'South Africa',
+		countryCode: 'ZA',
+		temp: 17,
+		lat: -33.928992,
+		lon: 18.417396
+	},
+	{
+		name: 'Johannesburg',
+		country: 'South Africa',
+		countryCode: 'ZA',
+		temp: 16,
+		lat: -26.195246,
+		lon: 28.034088
+	},
+	{
+		name: 'Lagos',
+		country: 'Nigeria',
+		countryCode: 'NG',
+		temp: 27,
+		lat: 6.4550575,
+		lon: 3.3941795
+	},
+	{
+		name: 'Nairobi',
+		country: 'Kenya',
+		countryCode: 'KE',
+		temp: 19,
+		lat: -1.2832533,
+		lon: 36.8172449
+	},
+	{
+		name: 'Casablanca',
+		country: 'Morocco',
+		countryCode: 'MA',
+		temp: 18,
+		lat: 33.5950627,
+		lon: -7.6187768
+	},
+	{
+		name: 'Tunis',
+		country: 'Tunisia',
+		countryCode: 'TN',
+		temp: 19,
+		lat: 36.8002068,
+		lon: 10.1857329
+	},
+	{
+		name: 'Algiers',
+		country: 'Algeria',
+		countryCode: 'DZ',
+		temp: 18,
+		lat: 36.7755648,
+		lon: 3.0597746
+	},
+	{
+		name: 'Melbourne',
+		country: 'Australia',
+		countryCode: 'AU',
+		temp: 15,
+		lat: -37.8142176,
+		lon: 144.9631608
+	},
+	{
+		name: 'Perth',
+		country: 'Australia',
+		countryCode: 'AU',
+		temp: 18,
+		lat: -31.9558933,
+		lon: 115.8605855
+	},
+	{
+		name: 'Auckland',
+		country: 'New Zealand',
+		countryCode: 'NZ',
+		temp: 15,
+		lat: -36.8485954,
+		lon: 174.7633732
+	},
+	{
+		name: 'Wellington',
+		country: 'New Zealand',
+		countryCode: 'NZ',
+		temp: 13,
+		lat: -41.2887953,
+		lon: 174.7772114
+	},
+	// Additional Middle East cities
+	{
+		name: 'Doha',
+		country: 'Qatar',
+		countryCode: 'QA',
+		temp: 32,
+		lat: 25.2856329,
+		lon: 51.5264162
+	},
+	{
+		name: 'Kuwait City',
+		country: 'Kuwait',
+		countryCode: 'KW',
+		temp: 30,
+		lat: 29.3796532,
+		lon: 47.9734174
+	},
+	{
+		name: 'Manama',
+		country: 'Bahrain',
+		countryCode: 'BH',
+		temp: 29,
+		lat: 26.1921769,
+		lon: 50.5354606
+	},
+	{
+		name: 'Muscat',
+		country: 'Oman',
+		countryCode: 'OM',
+		temp: 28,
+		lat: 23.5882019,
+		lon: 58.3829448
+	},
+	{
+		name: 'Abu Dhabi',
+		country: 'UAE',
+		countryCode: 'AE',
+		temp: 31,
+		lat: 24.4538352,
+		lon: 54.3774014
+	},
+	{
+		name: 'Amman',
+		country: 'Jordan',
+		countryCode: 'JO',
+		temp: 18,
+		lat: 31.9515694,
+		lon: 35.9239625
+	},
+	{
+		name: 'Damascus',
+		country: 'Syria',
+		countryCode: 'SY',
+		temp: 17,
+		lat: 33.5130695,
+		lon: 36.3095814
+	},
+	{
+		name: 'Beirut',
+		country: 'Lebanon',
+		countryCode: 'LB',
+		temp: 20,
+		lat: 33.8959203,
+		lon: 35.47843
+	},
+	{
+		name: 'Baghdad',
+		country: 'Iraq',
+		countryCode: 'IQ',
+		temp: 23,
+		lat: 33.3024309,
+		lon: 44.3787992
+	},
+	// Additional African cities
+	{
+		name: 'Addis Ababa',
+		country: 'Ethiopia',
+		countryCode: 'ET',
+		temp: 16,
+		lat: 9.0244611,
+		lon: 38.7467134
+	},
+	{
+		name: 'Khartoum',
+		country: 'Sudan',
+		countryCode: 'SD',
+		temp: 29,
+		lat: 15.5518544,
+		lon: 32.5324916
+	},
+	{
+		name: 'Accra',
+		country: 'Ghana',
+		countryCode: 'GH',
+		temp: 27,
+		lat: 5.5571096,
+		lon: -0.2012376
+	},
+	{
+		name: 'Abidjan',
+		country: 'Ivory Coast',
+		countryCode: 'CI',
+		temp: 26,
+		lat: 5.3364407,
+		lon: -4.0266972
+	},
+	{
+		name: 'Dakar',
+		country: 'Senegal',
+		countryCode: 'SN',
+		temp: 24,
+		lat: 14.6976414,
+		lon: -17.4407495
+	},
+	{
+		name: 'Kinshasa',
+		country: 'Democratic Republic of Congo',
+		countryCode: 'CD',
+		temp: 25,
+		lat: -4.3217055,
+		lon: 15.3125974
+	},
+	{
+		name: 'Luanda',
+		country: 'Angola',
+		countryCode: 'AO',
+		temp: 24,
+		lat: -8.8159898,
+		lon: 13.2306684
+	},
+	{
+		name: 'Maputo',
+		country: 'Mozambique',
+		countryCode: 'MZ',
+		temp: 23,
+		lat: -25.9686162,
+		lon: 32.5804042
+	},
+	{
+		name: 'Harare',
+		country: 'Zimbabwe',
+		countryCode: 'ZW',
+		temp: 18,
+		lat: -17.8178443,
+		lon: 31.0447142
+	},
+	{
+		name: 'Lusaka',
+		country: 'Zambia',
+		countryCode: 'ZM',
+		temp: 20,
+		lat: -15.3982237,
+		lon: 28.3228165
+	},
+	{
+		name: 'Kampala',
+		country: 'Uganda',
+		countryCode: 'UG',
+		temp: 22,
+		lat: 0.3177137,
+		lon: 32.5813539
 	}
 ];
 
